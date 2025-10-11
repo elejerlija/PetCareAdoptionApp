ee27b100aa6cdc8e0d0f060ea961c3915fff250e
elejerlija <elajerlija3@gmail.com>
2025-10-11 21:56:04 +0200

    testim i addpet

---
+++ app/AddPet.jsx
@@ -0,0 +1,11 @@
+// app/AddPet.jsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AddPet() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Add Pet Page</Text>
+    </View>
+  );
+}
