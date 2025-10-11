73876cb00523e38f0e0353877c666049a1305008
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2025-10-11 21:09:08 +0200

    Shtimi i city ne pets.js

--- assets/data/pets.js
+++ assets/data/pets.js
@@ -3,6 +3,7 @@ export const pets = [
     id: '1',
     name: 'Max',
     type: 'dog',
+    city: 'Prishtina',
     age: 3,
     desc: 'Energjik dhe miqësor',
     image: require('../images/dog1.jpg')
@@ -11,6 +12,7 @@ export const pets = [
     id: '2',
     name: 'Luna',
     type: 'cat',
+    city: 'Malisheve',
     age: 2,
     desc: 'Qetë dhe dashuruese',
     image: require('../images/cat1.jpg')
@@ -19,6 +21,7 @@ export const pets = [
     id: '3',
     name: 'Rocky',
     type: 'dog',
+    city: 'Gjilan',
     age: 5,
     desc: 'Besnik dhe mbrojtës',
     image: require('../images/dog2.jpg')
@@ -27,6 +30,7 @@ export const pets = [
     id: '4',
     name: 'Mia',
     type: 'cat',
+    city: 'Kaçanik',
     age: 1,
     desc: 'Lojtare dhe kurioze',
     image: require('../images/cat2.jpg')
@@ -35,6 +39,7 @@ export const pets = [
     id: '5',
     name: 'Nina',
     type: 'cat',
+    city: 'Prishtina',
     age: 4,
     desc: 'Inteligjente dhe e dashur',
     image: require('../images/cat3.jpg')
@@ -43,6 +48,7 @@ export const pets = [
     id: '6',
     name: 'Simba',
     type: 'cat',
+    city: 'Kaçanik',
     age: 2,
     desc: 'Sovran dhe elegant',
     image: require('../images/cat4.jpg')
