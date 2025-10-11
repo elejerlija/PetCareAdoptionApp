934b5ca88e4fb3c6d0a704d0e2a077eac22e4060
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2025-10-11 20:04:57 +0200

    pets.js dhe organizimi i fileve

---
+++ assets/data/pets.js
@@ -0,0 +1,50 @@
+export const demoPets = [
+  {
+    id: '1',
+    name: 'Max',
+    type: 'dog',
+    age: 3,
+    desc: 'Energjik dhe miqësor',
+    image: require('../images/dog1.jpg')
+  },
+  {
+    id: '2',
+    name: 'Luna',
+    type: 'cat',
+    age: 2,
+    desc: 'Qetë dhe dashuruese',
+    image: require('../images/cat1.jpg')
+  },
+  {
+    id: '3',
+    name: 'Rocky',
+    type: 'dog',
+    age: 5,
+    desc: 'Besnik dhe mbrojtës',
+    image: require('../images/dog2.jpg')
+  },
+  {
+    id: '4',
+    name: 'Mia',
+    type: 'cat',
+    age: 1,
+    desc: 'Lojtare dhe kurioze',
+    image: require('../images/cat2.jpg')
+  },
+  {
+    id: '5',
+    name: 'Nina',
+    type: 'cat',
+    age: 4,
+    desc: 'Inteligjente dhe e dashur',
+    image: require('../images/cat3.jpg')
+  },
+  {
+    id: '6',
+    name: 'Simba',
+    type: 'cat',
+    age: 2,
+    desc: 'Sovran dhe elegant',
+    image: require('../images/cat4.jpg')
+  }
+];
