80e2e9697456e69d0116b03c4f9e930e1a8187e5
erinemujku <erinamujku@gmail.com>
2025-10-11 22:06:22 +0200

    krijimi i konstantave

--- app/AddPet.jsx
+++ app/AddPet.jsx
@@ -1,8 +1,20 @@
-// app/AddPet.jsx
-import React from 'react';
+import React, { use, useState } from 'react';
 import { View, Text } from 'react-native';
+import {useRouter} from "expo-router";
+import { usePets } from './context/PetsContext';
 
 export default function AddPet() {
+    const router=useRouter();
+    const { addPet }=usePets();
+
+    const[form,setForm]=useState({
+        name:"",
+        type:"",
+        age:"",
+        desc:"",
+        image:"",
+    });
+    const[erros,setErrors]=useState({});
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Add Pet Page</Text>
