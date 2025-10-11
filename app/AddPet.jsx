5e968079189e0857fd09011a5d118a7e3264f937
erinemujku <erinamujku@gmail.com>
2025-10-11 22:11:21 +0200

    Validimi i te dhenave

--- app/AddPet.jsx
+++ app/AddPet.jsx
@@ -14,7 +14,27 @@ export default function AddPet() {
         desc:"",
         image:"",
     });
-    const[erros,setErrors]=useState({});
+    const[errors,setErrors]=useState({});
+
+    const validate = ()=> {
+        let valid=true;
+        let newErrors={};
+        
+        if(!form.name.trim()|| form.name.length<3){
+            newErrors.name="Emri duhet te kete te pakten 3 shkronja.";
+            valid=false;
+        }
+        if(!form.age.trim()|| isNaN(form.age)){
+            newErrors.age="Mosha duhet te jete numer.";
+            valid=false;
+        }
+        if(!form.desc.trim()||form.desc.length< 5){
+            newErrors.desc="Pershkrimi duhet te kete te pakten 5 karaktere.";
+            valid=false;
+        }
+        setErrors(newErrors);
+        return valid;
+    };
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Add Pet Page</Text>
