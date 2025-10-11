2d9c541d938669bc65fcc8c422263fa6ec2b4ee5
erinemujku <erinamujku@gmail.com>
2025-10-11 22:46:21 +0200

    Shtimi i TextInput

--- app/AddPet.jsx
+++ app/AddPet.jsx
@@ -1,7 +1,8 @@
 import React, { use, useState } from 'react';
-import { View, Text, Alert } from 'react-native';
+import { View, Text, Alert, Touchable, TouchableOpacity,ScrollView } from 'react-native';
 import {useRouter} from "expo-router";
 import { usePets } from './context/PetsContext';
+import { ScrollView, TextInput } from 'react-native-web';
 
 export default function AddPet() {
     const router=useRouter();
@@ -52,8 +53,37 @@ export default function AddPet() {
     const handleChange=(key,value)=>setForm({...form,[key]:value});
 
   return (
+    <ScrollView contentContainerStyle={styles.container}>
+
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Add Pet Page</Text>
     </View>
+
+    <Text style={styles.title}>Shto nje kafshe te re.</Text>
+
+    <TextInput style={styles.input} placeholder="Emri i kafshes" value={form.name} 
+    onChangeText={(v)=>handleChange("name",v)}/>
+    {errors.name && <Text style={styles.error}>{errors.name}</Text>}
+
+    <TextInput style={styles.input} placeholder="Tipi (qen,mace,...)"
+    value={form.type} onChangeText={(v)=>handleChange("type",v)}/>
+    {errors.type && <Text style={styles.error}>{errors.type}</Text>}
+
+    <TextInput style={styles.input} placeholder="Mosha (numer)" 
+    value={form.age} keyboardType="numeric"
+    onChangeText={(v)=>handleChange("age",v)}/>
+    {errors.age && <Text style={styles.error}>{errors.age}</Text>}
+
+    <TextInput style={[styles.input,{height:80}]}
+    placeholder="Pershkrim" value={form.desc} multiline onChangeText={(v)=> handleChange("desc",v)}/>
+    {errors.desc && <Text style={styles.error}>{errors.desc}</Text>}
+
+    <TextInput style={styles.input} placeholder="Image URL (opsionale)"
+    value={form.image} onChangeText={(v) => handleChange("image", v)}/>
+   
+    <TouchableOpacity style={styles.button} onPress={handleAdd}>
+        <Text style={styles.buttonText}>Shto Kafshen</Text>
+    </TouchableOpacity> 
+    </ScrollView>
   );
 }
