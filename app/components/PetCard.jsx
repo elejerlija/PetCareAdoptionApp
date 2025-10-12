03ba9351151efc8ff9ea09f8dc292d3baa0bdaf6
elejerlija <elajerlija3@gmail.com>
2025-10-12 14:25:54 +0200

    Update PetCard.jsx

--- app/components/PetCard.jsx
+++ app/components/PetCard.jsx
@@ -11,30 +11,30 @@ export default function PetCard({ pet, onPress }) {
     >
       <Image source={pet.image} style={styles.image} />
       <View style={styles.infoContainer}>
-  <Text style={styles.name}>{pet.name}</Text>
+        <Text style={styles.name}>{pet.name}</Text>
 
-  <View style={styles.metaRow}>
-    <Text style={styles.details}>
-      {pet.age} yr{pet.age === 1 ? '' : 's'} · {pet.city}
-    </Text>
+        <View style={styles.metaRow}>
+          <Text style={styles.details}>
+            {pet.age} yr{pet.age === 1 ? '' : 's'} · {pet.city}
+          </Text>
 
-    <View
-      style={[
-        styles.badge,
-        { backgroundColor: pet.available ? '#DCFCE7' : '#FEE2E2' },
-      ]}
-    >
-      <Text
-        style={[
-          styles.badgeText,
-          { color: pet.available ? '#16A34A' : '#DC2626' },
-        ]}
-      >
-        {pet.available ? 'Available' : 'Not available'}
-      </Text>
-    </View>
-  </View>
-</View>
+          <View
+            style={[
+              styles.badge,
+              { backgroundColor: pet.available ? '#DCFCE7' : '#FEE2E2' },
+            ]}
+          >
+            <Text
+              style={[
+                styles.badgeText,
+                { color: pet.available ? '#62b681ff' : '#da3333ff' },
+              ]}
+            >
+              {pet.available ? 'Available' : 'Not available'}
+            </Text>
+          </View>
+        </View>
+      </View>
 
     </TouchableOpacity>
 
@@ -51,10 +51,10 @@ const styles = StyleSheet.create({
     width: '85%',
     marginBottom: 12,
     elevation: 3,
-          shadowColor: "#000",
-          shadowOpacity: 0.15,
-          shadowRadius: 8,
-          shadowOffset: { width: 0, height: 4 }
+    shadowColor: "#000",
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 4 }
   },
   image: {
     width: 60,
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     borderRadius: 30
   },
   infoContainer: {
-      flex: 1,
+    flex: 1,
     marginLeft: 12
   },
   name: {
@@ -73,24 +73,25 @@ const styles = StyleSheet.create({
     color: 'gray'
   },
   metaRow: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  marginTop: 2,
-},
-details: {
-  color: '#6B7280',
-  fontSize: 14,
-},
-badge: {
-  paddingHorizontal: 10,
-  paddingVertical: 4,
-  borderRadius: 999, 
-  minWidth: 80,
-  alignItems: 'center'},
-badgeText: {
-  fontSize: 12,
-  fontWeight: '600',
-}
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 2,
+  },
+  details: {
+    color: '#6B7280',
+    fontSize: 14,
+  },
+  badge: {
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    borderRadius: 999,
+    minWidth: 80,
+    alignItems: 'center'
+  },
+  badgeText: {
+    fontSize: 12,
+    fontWeight: '600',
+  }
 
 });
