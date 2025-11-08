f21a49616841fab3b4574254f0e1821dc3066f0e
elejerlija <elajerlija3@gmail.com>
2025-11-08 20:10:50 +0100

    added firebase connection and testing setup

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-g_w9B4Nin7TqAtb487dfX3bI1mBmOpo",
+  authDomain: "pet-care-adoption.firebaseapp.com",
+  projectId: "pet-care-adoption",
+  storageBucket: "pet-care-adoption.firebasestorage.app",
+  messagingSenderId: "206997293389",
+  appId: "1:206997293389:web:c49a074876195a46618f04",
+  measurementId: "G-GRV1DKDJP2"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
