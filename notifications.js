e5535bcfaa310017d82bd5348948c0d9504e8d96
FahrijeGjoka <fahrije.gjokiqi@student.uni-pr.edu>
2025-12-04 15:02:33 +0100

    Shtimi i file per notifications

---
+++ notifications.js
@@ -0,0 +1,22 @@
+import * as Notifications from 'expo-notifications';    
+import { Alert } from 'react-native';
+import {Platform} from 'react-native';
+
+export async function registerPushNotifications() {
+    const {status} = await Notifications.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+        Alert.alert("Permission required", "Push notifications permission is required to receive updates.");
+        return;
+    }
+
+    if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default notifications go here",
+      importance: Notifications.AndroidImportance.MAX,
+    });
+  }
+
+  return true;
+
+}
